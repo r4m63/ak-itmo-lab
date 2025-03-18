6acbe70c045f1ba6dd14077275ce04e9dc75f9dc
rmtj <rm.tj.777@gmail.com>
2025-03-18 16:17:11 +0300

    f32a architecture task

--- f32a/sum.s
+++ f32a/sum.s
@@ -6,49 +6,45 @@ overflow_value: .word 0xCCCCCCCC
 
 .text
 
-read_address:   \ read_address(addr)
+read_address:
     a!
     @
     ;
 
-write_address:  \ write_address(addr, value)
+write_address:
     a!
     !
     ;
 
-mul:    \ (a, b) -> a*b, a - value in A register, b - second value from top of the stack
-    lit 0           \ High part of the multiplication result (NOT USED)
+mul:
+    lit 0
     lit 31
     >r
-mul_do:
+mul_loop:
     +*
-    next mul_do
+    next mul_loop
     over
     drop
-    if ok_1
-    
+    if mul_no_overflow
     @p overflow_value
     ;
-ok_1:
+mul_no_overflow:
     a
-
     dup
-    -if ok_2
-    
+    -if mul_no_negative
     drop 
     @p overflow_value
     ;
-
-ok_2:
+mul_no_negative:
     ;
 
-square: \ x*x
+square:
     dup
     a!
     mul
     ;
 
-is_valid:   \is_valid(n), checks if n > 0, returns 0 if yes, -1 otherwise
+is_valid:
     dup
     lit -1
     + 
@@ -60,23 +56,20 @@ true:
     lit 0
     ;
 
-compare:    \ compare(a, b), returns 0 if a == b, 1 otherwise, doesn't duplicate stack values
+compare:
     inv
     lit 1
     +
     +
-
     if equal
 not_equal:
     lit 1 ;
 equal:
     lit 0 ;
-    
 
 _start:
     @p input_address
     read_address
-
     is_valid 
     if good_value
 bad_value:
@@ -86,14 +79,10 @@ good_value:
     2/
     dup
     square
-
-    \ if (stack.top() == overflow_value)
     dup
     @p overflow_value
     compare
-
     if overflow
-
     +
     end
     ;
