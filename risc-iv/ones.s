a25f4e82da37b20cc2cf87402d86b1212c0c9f3d
ramil <rm.tj.777@gmail.com>
2025-03-30 04:09:14 +0300

    risc-iv-32 lab3 comments

--- risc-iv/ones.s
+++ risc-iv/ones.s
@@ -5,24 +5,24 @@ output_address:  .word  0x84
     .text
 
 _start:
-    lui      t0, %hi(input_address)          ; t0 = 0
-    addi     t0, t0, %lo(input_address)      ; t0 <- 80
+    lui      t0, %hi(input_address)
+    addi     t0, t0, %lo(input_address)
 
-    lw       t0, 0(t0)                       ; t0 = *input_address
-    lw       t0, 0(t0)                       ; t0 = *t0
+    lw       t0, 0(t0)
+    lw       t0, 0(t0)
 
-    addi     t1, zero, 1                     ; using t1 as mask
-    addi     t4, zero, 1                     ; using t4 as one, cuz heres no immediate loads
+    addi     t1, zero, 1
+    addi     t4, zero, 1
 loop:
-    beqz     t1, end                         ; if mask is zero, means we checked all bits
-    and      t2, t0, t1                      ; doing and between mask and our value
-    sll      t1, t1, t4                      ; shifting mask
-    beqz     t2, loop                        ; if t2 is zero, means bcurrent bit is 0
-    add      t3, t3, t4                      ; otherwise incrementing anwser
-    j        loop                            ; jumping to the loop begining
+    beqz     t1, end
+    and      t2, t0, t1
+    sll      t1, t1, t4
+    beqz     t2, loop
+    add      t3, t3, t4
+    j        loop
 end:
-    lui      t0, %hi(output_address)         ; t0 = 0
-    addi     t0, t0, %lo(output_address)     ; t0 <- 84
-    lw       t0, 0(t0)                       ; t0 = *output_address
-    sw       t3, 0(t0)                       ; *t0 = t3 (answer)
+    lui      t0, %hi(output_address)
+    addi     t0, t0, %lo(output_address)
+    lw       t0, 0(t0)
+    sw       t3, 0(t0)
     halt
